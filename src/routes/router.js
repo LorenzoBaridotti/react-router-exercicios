46e7be869370b89d93a28a99af08277f250b384f
Lorenzo Baridotti <lorenzo.baridotti@hotmai.com>
2023-01-10 19:45:31 -0300

    exercicios router

--- src/routes/router.js
+++ src/routes/router.js
@@ -13,8 +13,8 @@ const Router = () => {
         <BrowserRouter>
         <Routes>
             <Route index element={<HomePage/>}/>
-            <Route path="/profile/:name" element={<ProfilePage/>}/>
-            <Route path="/error" element={<ErrorPage/>}/>
+            <Route path="/profile/:id" element={<ProfilePage/>}/>
+            <Route path="*" element={<ErrorPage/>}/>
             <Route path="/product/:id" element={<ProductPage/>}/>
         </Routes>
         </BrowserRouter>
